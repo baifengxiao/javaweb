19a0186a23ba2d160febfadce03fc98290bfaa0f
yupengtao <javaer@111.com>
2023-07-02 14:09:49 +0800

    mvc思想2：反射替代switch case

--- src/main/java/cn/sc/love/thymeleaf/servlet/ServletOptimize.java
+++ src/main/java/cn/sc/love/thymeleaf/servlet/ServletOptimize.java
@@ -15,6 +15,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static com.sun.javafx.fxml.expression.Expression.add;
 
@@ -28,50 +30,68 @@ public class ServletOptimize extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        req.setAttribute("operate", "D");
+        req.setAttribute("operate", "C");
         String operate = (String) req.getAttribute("operate");
         System.out.println("当前操作是：" + operate);
-        if (StringUtil.isNotEmpty(operate)) {
-            switch (operate) {
-                case "C":
-                    C(req, resp);
-                    break;
 
-                case "R":
-                    R(req, resp);
-                    break;
-                case "U":
-                    U(req, resp);
-                    break;
-                case "D":
-                    D(req, resp);
-                    break;
-                default:
-                    throw new RuntimeException("operation值非法!");
+
+        Method[] methods = this.getClass().getDeclaredMethods();
+        for (Method method : methods) {
+            String methodName = method.getName();
+            if (methodName.equals(operate)) {
+                try {
+                    method.invoke(this, req, resp);
+                    return;
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                } catch (InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
             }
+
         }
 
+        //StreamApi写法
+        //TODO foreach是消费型接口，没法用return中止程序
+//        Arrays.stream(methods).forEach(method -> {
+//            String methodName = method.getName();
+//
+//            if (methodName.equals(operate)) {
+//                try {
+//                    method.invoke(this, req, resp);
+//
+//                } catch (IllegalAccessException e) {
+//                    throw new RuntimeException(e);
+//                } catch (InvocationTargetException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            } else {
+//                throw new RuntimeException("operation值非法!");
+//            }
+//
+//        });
+
 
     }
 
 
-    private void C(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("C...");
-    }
-
-
-    private void R(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("R...");
-
-    }
-
-    private void U(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("U...");
-    }
-
-    private void D(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("D...");
-    }
+//    private void C(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.out.println("C...");
+//    }
+//
+//
+//    private void R(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.out.println("R...");
+//
+//    }
+//
+//    private void U(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.out.println("U...");
+//    }
+//
+//    private void D(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.out.println("D...");
+//    }
 
 
 }
