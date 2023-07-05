d1683e819c2a24d6e9bba5c555a0d522217ae15d
yupengtao <javaer@111.com>
2023-07-06 07:58:47 +0800

    servlet初始化简单演示

--- src/main/java/cn/sc/love/Deme1Servlet.java
+++ src/main/java/cn/sc/love/Deme1Servlet.java
@@ -1,10 +1,14 @@
 package cn.sc.love;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @Author yupengtao
@@ -18,7 +22,24 @@ public class Deme1Servlet extends HttpServlet {
         ServletConfig config = getServletConfig();
         String initValue = config.getInitParameter("hello");
         System.out.println("initValue = " + initValue);
-        String uname= config.getInitParameter("uname");;
+        String uname = config.getInitParameter("uname");
+        ;
         System.out.println("uname = " + uname);
+
+        //演示下获取上下文参数，此时不用理解这是什么
+        ServletContext servletContext = getServletContext();
+        //初始化方法中可以获取
+        String contextConfigLocation = servletContext.getInitParameter("contextConfigLocation");
+        System.out.println("contextConfigLocation = " + contextConfigLocation);
+        //另外，req，session中也可以获取
+
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //另外，req，session中也可以获取
+        req.getServletContext();
+        req.getSession().getServletContext();
     }
 }
